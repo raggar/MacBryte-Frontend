2204ccd51e00a77e5e0752fbb09e9dd9a8c44de3
Rahul Aggarwal <rahulaggarwal1016@gmail.com>
2021-06-05 18:41:13 -0400

    Initial Commit

---
+++ MacBryte/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MacBryte
+//
+//  Created by Rahul Aggarwal on 2021-06-05.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
