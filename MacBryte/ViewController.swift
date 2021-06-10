431e209c6938da507de8ecb8105b450b5f85f1f5
Dhruv Rawat <dhruvrawat09@gmail.com>
2021-06-09 22:06:33 -0400

    Added hyperlinks and can now compose emails (#1)
    * Emails and other links work
    
    * config file;
    
    * Added ViewController.swift
    
    * Added guard statement

--- MacBryte/ViewController.swift
+++ MacBryte/ViewController.swift
@@ -8,17 +8,49 @@
 import Cocoa
 
 class ViewController: NSViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        print("view loaded")
     }
 
     override var representedObject: Any? {
         didSet {
         // Update the view, if already loaded.
         }
+    }
+    
+    /*
+     When the email on the app window is clicked
+     */
+    @IBAction func emailClicked(_ sender: Any) {
+        openAndComposeEmail()
+    }
+    
+    /*
+     When website link is pressed, redirects us to the macbryte website
+     */
+    @IBAction func macbryteLinkClicked(_ sender: Any) {
+        if let url = URL(string: "https://www.macbryte.com") {
+            NSWorkspace.shared.open(url)
+        }
+    }
+    
+    /*
+     When the "Contact MacBryte" button is pressed open the mail app to compose an email
+     */
+    @IBAction func contactMacBryte(_ sender: Any) {
+        openAndComposeEmail()
+    }
+    
+    func openAndComposeEmail() {
+        guard let service = NSSharingService(named: NSSharingService.Name.composeEmail) else {
+            print("Composing email failed")
+            return
+        }
+        
+        service.recipients = ["rahul@macbryte.com", "dhruv@macbryte.com"]
+        service.subject = "MacBryte App: Help Request"
+        service.perform(withItems: ["Please delete this placeholder and write your message here"])
     }
 }
 
